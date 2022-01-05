6d0e31068a46f90a343a9524390f75e1cf15e2cf
hato99 <72713637+hato99@users.noreply.github.com>
2022-01-06 00:55:29 +0300

    new

--- admin/src/firebase.js
+++ admin/src/firebase.js
@@ -5,12 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBS1wpGvSuy3IaeN0GLvgT-UqzBiMmf7oQ",
-  authDomain: "ecommerce-fe018.firebaseapp.com",
-  projectId: "ecommerce-fe018",
-  storageBucket: "ecommerce-fe018.appspot.com",
-  messagingSenderId: "902977294250",
-  appId: "1:902977294250:web:f8d45e668a4564b3c0e5d3"
+  
 };
 
 // Initialize Firebase
--- admin/src/requestMethods.js
+++ admin/src/requestMethods.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-  const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZDU5YWNhNTIwYzU0ZjA4MTdmMmY0ZiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MTQxMzkyMSwiZXhwIjoxNjQxNjczMTIxfQ.lytFugjsvU8Ylgz7DzWAQzVrNSTwWSleHakvWqTHZSs";
-  // JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
+  const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
